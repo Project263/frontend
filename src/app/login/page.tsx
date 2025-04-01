d65a44f270218816fb2e6a3e20f37efad1670bfc
Ruslan Lukoyanov <ruslan.ru226@mail.ru>
2025-04-01 18:49:13 +0400

    main: update url

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -31,7 +31,7 @@ export default function Example() {
             </div>
             <div className="flex flex-col items-center justify-center">
               <a
-                href="http://localhost:8080/auth/google"
+                href="https://sso.theaesthetics.ru/auth/google"
                 className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-orange-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline"
               >
                 <div className="bg-white p-2 rounded-full">
